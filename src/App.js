d18489e995efcdb90e45796c89a24d82c8551b00
José Augusto Nascimento Rosa <augusto.rosarj@gmail.com>
2023-11-09 21:48:45 -0300

    novidades

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header'
 import Name from './components/name'
 import Sobre from './components/sobremim'
 import Experiencia from './components/experiencia'
+import Skills from './components/skills'
 function App() {  
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Name/>
       <Sobre/>
       <Experiencia/>
+      <Skills/>
     </div>
   );
 }
---
+++ src/components/skills.jsx
@@ -0,0 +1,35 @@
+import './skills.css';
+
+function Skills() {
+  return (
+    <div className="skillssDiv">
+        <div className='Blurr'>
+        <div>
+            <h3 className='titleSKIL'>Habilidades</h3>
+        </div>
+        <div>
+           <ul className='listaIcons'>
+            <li className='react'></li>
+            <li className='python'></li>
+            <li className='vue'></li>
+            <li className='jquery'></li>
+            <li className='js'></li>
+            <li className='nest'></li>
+            <li className='sql'></li>
+            <li className='mongodb'></li>
+            <li className='lutter'></li>
+            <li className='kotlin'></li>
+            <li className='c'></li>
+            <li className='java'></li>
+            <li className='git'></li>
+            <li className='figma'></li>
+            
+           
+           </ul>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default Skills;
